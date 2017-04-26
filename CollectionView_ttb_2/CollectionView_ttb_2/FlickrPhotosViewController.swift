4cf4a1d4e58e19aace7f00d47f9712dd066fb81b
thanhbinh13131 <thanhbinh13131@gmail.com>
2017-04-26 15:47:52 +0700

    Add files via upload

---
+++ CollectionView_ttb_2/CollectionView_ttb_2/FlickrPhotosViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FlickrPhotosViewController.swift
+//  CollectionView_ttb_2
+//
+//  Created by admin25 on 4/26/17.
+//  Copyright © 2017 admin25. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+final class FlickrPhotosViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    fileprivate let reuseIdentifier = "FlickrCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    fileprivate var searches = [FlickrSearchResults]()
+    fileprivate let flickr = Flickr()
+    
+    }
+
+// MARK: - Private
+private extension FlickrPhotosViewController {
+    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+    }
+}
+
+extension FlickrPhotosViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 1
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            
+            
+            activityIndicator.removeFromSuperview()
+            
+            
+            if let error = error {
+                // 2
+                print("Error searching : \(error)")
+                return
+            }
+            
+            if let results = results {
+                // 3
+                print("Found \(results.searchResults.count) matching \(results.searchTerm)")
+                self.searches.insert(results, at: 0)
+                
+                // 4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
---
+++ CollectionView_ttb_2/CollectionView_ttb_2Tests/CollectionView_ttb_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionView_ttb_2Tests.swift
+//  CollectionView_ttb_2Tests
+//
+//  Created by admin25 on 4/26/17.
+//  Copyright © 2017 admin25. All rights reserved.
+//
+
+import XCTest
+@testable import CollectionView_ttb_2
+
+class CollectionView_ttb_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
